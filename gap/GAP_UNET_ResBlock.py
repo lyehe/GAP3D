efa6b3f7d6073e9b22e18f963c5b75bf74e24826
lyehe <yxl448@case.edu>
2023-10-01 09:13:05 -0400

    Formating improvment

--- gap/GAP_UNET_ResBlock.py
+++ gap/GAP_UNET_ResBlock.py
@@ -40,12 +40,11 @@ import torch.optim as optim
 import pytorch_lightning as pl
 
 
-
 from torch.nn import init
 import numpy as np
 
-def conv3x3(in_channels, out_channels, stride=1, 
-            padding=1, bias=True, groups=1):    
+
+def conv3x3(in_channels, out_channels, stride=1, padding=1, bias=True, groups=1):
     return nn.Conv2d(
         in_channels,
         out_channels,
@@ -53,29 +52,24 @@ def conv3x3(in_channels, out_channels, stride=1,
         stride=stride,
         padding=padding,
         bias=bias,
-        groups=groups)
+        groups=groups,
+    )
 
-def upconv2x2(in_channels, out_channels, mode='transpose'):
-    if mode == 'transpose':
-        return nn.ConvTranspose2d(
-            in_channels,
-            out_channels,
-            kernel_size=2,
-            stride=2)
+
+def upconv2x2(in_channels, out_channels, mode="transpose"):
+    if mode == "transpose":
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
     else:
         # out_channels is always going to be the same
         # as in_channels
         return nn.Sequential(
-            nn.Upsample(mode='bilinear', scale_factor=2),
-            conv1x1(in_channels, out_channels))
+            nn.Upsample(mode="bilinear", scale_factor=2),
+            conv1x1(in_channels, out_channels),
+        )
+
 
 def conv1x1(in_channels, out_channels, groups=1):
-    return nn.Conv2d(
-        in_channels,
-        out_channels,
-        kernel_size=1,
-        groups=groups,
-        stride=1)
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, groups=groups, stride=1)
 
 
 class DownConv(nn.Module):
@@ -83,6 +77,7 @@ class DownConv(nn.Module):
     A helper Module that performs 2 convolutions and 1 MaxPool.
     A ReLU activation follows each convolution.
     """
+
     def __init__(self, in_channels, out_channels, pooling=True):
         super(DownConv, self).__init__()
 
@@ -101,7 +96,7 @@ class DownConv(nn.Module):
         xskip = self.conv1(x)
         x = F.relu(self.conv2(xskip))
         x = F.relu(self.conv3(x) + xskip)
-#         x = F.dropout(x)
+        #         x = F.dropout(x)
         before_pool = x
         if self.pooling:
             x = self.pool(x)
@@ -113,8 +108,10 @@ class UpConv(nn.Module):
     A helper Module that performs 2 convolutions and 1 UpConvolution.
     A ReLU activation follows each convolution.
     """
-    def __init__(self, in_channels, out_channels, 
-                 merge_mode='concat', up_mode='transpose'):
+
+    def __init__(
+        self, in_channels, out_channels, merge_mode="concat", up_mode="transpose"
+    ):
         super(UpConv, self).__init__()
 
         self.in_channels = in_channels
@@ -122,39 +119,36 @@ class UpConv(nn.Module):
         self.merge_mode = merge_mode
         self.up_mode = up_mode
 
-        self.upconv = upconv2x2(self.in_channels, self.out_channels, 
-            mode=self.up_mode)
+        self.upconv = upconv2x2(self.in_channels, self.out_channels, mode=self.up_mode)
 
-        if self.merge_mode == 'concat':
-            self.conv1 = conv3x3(
-                2*self.out_channels, self.out_channels)
+        if self.merge_mode == "concat":
+            self.conv1 = conv3x3(2 * self.out_channels, self.out_channels)
         else:
             # num of input channels to conv2 is same
             self.conv1 = conv3x3(self.out_channels, self.out_channels)
         self.conv2 = conv3x3(self.out_channels, self.out_channels)
         self.conv3 = conv3x3(self.out_channels, self.out_channels)
 
-
     def forward(self, from_down, from_up):
-        """ Forward pass
+        """Forward pass
         Arguments:
             from_down: tensor from the encoder pathway
             from_up: upconv'd tensor from the decoder pathway
         """
         from_up = self.upconv(from_up)
-        if self.merge_mode == 'concat':
+        if self.merge_mode == "concat":
             x = torch.cat((from_up, from_down), 1)
         else:
             x = from_up + from_down
         xskip = self.conv1(x)
         x = F.relu(self.conv2(xskip))
         x = F.relu(self.conv3(x) + xskip)
-#         x = F.dropout(x)
+        #         x = F.dropout(x)
         return x
 
 
 class UN(pl.LightningModule):
-    """ `UNet` class is based on https://arxiv.org/abs/1505.04597
+    """`UNet` class is based on https://arxiv.org/abs/1505.04597
     The U-Net is a convolutional encoder-decoder neural network.
     Contextual spatial information (from the decoding,
     expansive pathway) about an input tensor is merged with
@@ -174,47 +168,58 @@ class UN(pl.LightningModule):
         the tranpose convolution (specified by upmode='transpose')
     """
 
-    def __init__(self, levels, channels=3, depth=5, 
-                 start_filts=64, up_mode='transpose', 
-                 merge_mode='add'):
+    def __init__(
+        self,
+        levels,
+        channels=3,
+        depth=5,
+        start_filts=64,
+        up_mode="transpose",
+        merge_mode="add",
+    ):
         """
         Arguments:
             in_channels: int, number of channels in the input tensor.
                 Default is 3 for RGB images.
             depth: int, number of MaxPools in the U-Net.
-            start_filts: int, number of convolutional filters for the 
+            start_filts: int, number of convolutional filters for the
                 first conv.
             up_mode: string, type of upconvolution. Choices: 'transpose'
                 for transpose convolution or 'upsample' for nearest neighbour
                 upsampling.
         """
         self.save_hyperparameters()
-        
+
         super(UN, self).__init__()
-        
-        
-        if up_mode in ('transpose', 'upsample'):
+
+        if up_mode in ("transpose", "upsample"):
             self.up_mode = up_mode
         else:
-            raise ValueError("\"{}\" is not a valid mode for "
-                             "upsampling. Only \"transpose\" and "
-                             "\"upsample\" are allowed.".format(up_mode))
-    
-        if merge_mode in ('concat', 'add'):
+            raise ValueError(
+                '"{}" is not a valid mode for '
+                'upsampling. Only "transpose" and '
+                '"upsample" are allowed.'.format(up_mode)
+            )
+
+        if merge_mode in ("concat", "add"):
             self.merge_mode = merge_mode
         else:
-            raise ValueError("\"{}\" is not a valid mode for"
-                             "merging up and down paths. "
-                             "Only \"concat\" and "
-                             "\"add\" are allowed.".format(up_mode))
+            raise ValueError(
+                '"{}" is not a valid mode for'
+                "merging up and down paths. "
+                'Only "concat" and '
+                '"add" are allowed.'.format(up_mode)
+            )
 
         # NOTE: up_mode 'upsample' is incompatible with merge_mode 'add'
-        if self.up_mode == 'upsample' and self.merge_mode == 'add':
-            raise ValueError("up_mode \"upsample\" is incompatible "
-                             "with merge_mode \"add\" at the moment "
-                             "because it doesn't make sense to use "
-                             "nearest neighbour to reduce "
-                             "depth channels (by half).")
+        if self.up_mode == "upsample" and self.merge_mode == "add":
+            raise ValueError(
+                'up_mode "upsample" is incompatible '
+                'with merge_mode "add" at the moment '
+                "because it doesn't make sense to use "
+                "nearest neighbour to reduce "
+                "depth channels (by half)."
+            )
 
         self.levels = levels
         self.channels = channels
@@ -227,21 +232,20 @@ class UN(pl.LightningModule):
         # create the encoder pathway and add to a list
         for i in range(depth):
             ins = self.channels * self.levels if i == 0 else outs
-            outs = self.start_filts*(2**i)
-#            outs = self.start_filts
-            pooling = True if i < depth-1 else False
+            outs = self.start_filts * (2**i)
+            #            outs = self.start_filts
+            pooling = True if i < depth - 1 else False
 
             down_conv = DownConv(ins, outs, pooling=pooling)
             self.down_convs.append(down_conv)
 
         # create the decoder pathway and add to a list
         # - careful! decoding only requires depth-1 blocks
-        for i in range(depth-1):
+        for i in range(depth - 1):
             ins = outs
             outs = ins // 2
-#            outs = ins
-            up_conv = UpConv(ins, outs, up_mode=up_mode,
-                merge_mode=merge_mode)
+            #            outs = ins
+            up_conv = UpConv(ins, outs, up_mode=up_mode, merge_mode=merge_mode)
             self.up_convs.append(up_conv)
 
         self.conv_final = conv1x1(outs, self.channels)
@@ -258,76 +262,81 @@ class UN(pl.LightningModule):
             init.xavier_normal(m.weight)
             init.constant(m.bias, 0)
 
-
     def reset_params(self):
         for i, m in enumerate(self.modules()):
             self.weight_init(m)
 
-
     def forward(self, x):
         epsilon = 1
-        
+
         stack = None
-        
+
         factor = 10.0
-        for i in range (self.levels):
-            scale = x.clone()*(factor**(-i))
+        for i in range(self.levels):
+            scale = x.clone() * (factor ** (-i))
             scale = torch.sin(scale)
             if stack is None:
                 stack = scale
             else:
-                stack = torch.cat((stack,scale),1)
-        
+                stack = torch.cat((stack, scale), 1)
+
         x = stack
-        
+
         encoder_outs = []
-         
+
         # encoder pathway, save outputs for merging
         for i, module in enumerate(self.down_convs):
             x, before_pool = module(x)
             encoder_outs.append(before_pool)
 
         for i, module in enumerate(self.up_convs):
-            before_pool = encoder_outs[-(i+2)]
+            before_pool = encoder_outs[-(i + 2)]
             x = module(before_pool, x)
-        
+
         x = self.conv_final(x)
         return x
 
-
-
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-4)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10)
-        
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode="min", factor=0.5, patience=10
+        )
+
         return {
-           'optimizer': optimizer,
-           'lr_scheduler': scheduler, 
-           'monitor': 'val_loss'
+            "optimizer": optimizer,
+            "lr_scheduler": scheduler,
+            "monitor": "val_loss",
         }
-    
-    
-    def photonLoss(self,result, target):
+
+    def photonLoss(self, result, target):
         expEnergy = torch.exp(result)
-        perImage =  -torch.mean(result*target, dim =(-1,-2,-3), keepdims = True )
-        perImage += torch.log(torch.mean(expEnergy, dim =(-1,-2,-3), keepdims = True ))*torch.mean(target, dim =(-1,-2,-3), keepdims = True )
+        perImage = -torch.mean(result * target, dim=(-1, -2, -3), keepdims=True)
+        perImage += torch.log(
+            torch.mean(expEnergy, dim=(-1, -2, -3), keepdims=True)
+        ) * torch.mean(target, dim=(-1, -2, -3), keepdims=True)
         return torch.mean(perImage)
-    
-    def MSELoss(self,result, target):
+
+    def MSELoss(self, result, target):
         expEnergy = torch.exp(result)
-        expEnergy /= (torch.mean(expEnergy, dim =(-1,-2,-3), keepdims = True ))
-        target = target / (torch.mean(target, dim =(-1,-2,-3), keepdims = True ))
-        return torch.mean((expEnergy-target)**2)
-    
+        expEnergy /= torch.mean(expEnergy, dim=(-1, -2, -3), keepdims=True)
+        target = target / (torch.mean(target, dim=(-1, -2, -3), keepdims=True))
+        return torch.mean((expEnergy - target) ** 2)
+
     def training_step(self, batch, batch_idx):
-        loss = self.photonLoss(self(batch[:,self.channels:,...]),batch[:,:self.channels,...] )
+        loss = self.photonLoss(
+            self(batch[:, self.channels :, ...]), batch[:, : self.channels, ...]
+        )
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self.photonLoss(self(batch[:,self.channels:,...]),batch[:,:self.channels,...] )
+        loss = self.photonLoss(
+            self(batch[:, self.channels :, ...]), batch[:, : self.channels, ...]
+        )
         self.log("val_loss", loss)
 
     def test_step(self, batch, batch_idx):
-        loss = self.photonLoss(self(batch[:,self.channels:,...]),batch[:,:self.channels,...] )
+        loss = self.photonLoss(
+            self(batch[:, self.channels :, ...]), batch[:, : self.channels, ...]
+        )
         self.log("test_loss", loss)
